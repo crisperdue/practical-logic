92ebf78d2a9f32b7f7866e0b68b7ea728634b806
Cris Perdue <cris@perdues.com>
2020-07-15 18:36:52 -0700

    Remove "printers.ml" from source control. It is a generated file.

--- printers.ml
+++
@@ -1,9 +0,0 @@
-initialization.ml:#install_printer print_num;;                           (* when printing nums *)
-lib.ml:#install_printer print_fpf;;
-intro.ml:#install_printer print_exp;;
-prop.ml:#install_printer print_prop_formula;;
-bdd.ml:#install_printer print_bdd;;
-fol.ml:#install_printer printert;;
-fol.ml:#install_printer print_fol_formula;;
-lcf.ml:#install_printer print_thm;;
-tactics.ml:#install_printer print_goal;;
